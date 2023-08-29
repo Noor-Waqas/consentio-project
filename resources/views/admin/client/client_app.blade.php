3d1c26111276acc47c7e5288cf33efd48fa741b0
Hafiz Nouman <hafiz9oman.dev@gmail.com>
2023-08-29 03:00:40 -0700

    Datatable issue solved in the Reports

--- resources/views/admin/client/client_app.blade.php
+++ resources/views/admin/client/client_app.blade.php
@@ -564,7 +564,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.20/b-1.6.1/b-html5-1.6.1/datatables.min.js"></script>
-<!-- @if(Request::segment(1) != 'report') -->
+
 <script>
     $(document).ready(function() {
 
@@ -582,7 +582,6 @@
         });
     });
     </script>
-<!-- @endif -->
 
     <script>
         // JavaScript function to toggle the collapse
--- resources/views/reports/audit_report_dash.blade.php
+++ resources/views/reports/audit_report_dash.blade.php
@@ -393,6 +393,7 @@ $busData = [
         $('.report-change').addClass('d-none');
         // Add Logo
         $('#report-logo').removeClass('d-none');
+        $('#myDiv').removeClass('mt-5');
         $('#myDiv').attr("style", "padding:7%;");
 
 
@@ -433,7 +434,8 @@ $busData = [
             $('.report-change').removeClass('d-none');
             // Remove Logo
             $('#report-logo').addClass('d-none');
-            $('#myDiv').attr("style", "padding:0;")
+            $('#myDiv').attr("style", "padding:0;");
+            $('#myDiv').addClass('mt-5');
 
             // Reinitialize the DataTable after capturing the screenshot
             initializeDataTable();
@@ -812,7 +814,7 @@ $(document).ready(function() {
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.20/b-1.6.1/b-html5-1.6.1/datatables.min.js"></script>
-@if(Request::segment(1) != 'dash')
+@if(Request::segment(1) == 'dash')
 <script>
     $(document).ready(function() {
 
